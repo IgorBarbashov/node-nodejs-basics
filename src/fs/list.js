4ce2b5d9837598439c4a9a0a19cb4c4a03f61db9
Igor Barbashov <bis200705@gmail.com>
2022-06-11 19:53:47 +0300

    feat: implement fs functions

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,31 @@
+import * as fs from 'fs/promises';
+import path from 'path';
+
+const __dirname = path.resolve(path.dirname(''));
+const folderToListPath = path.join(__dirname, 'files');
+
+export const doesExist = async (path) => {
+  try {
+    await fs.access(path);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
 export const list = async () => {
-    // Write your code here 
+  const folderExists = await doesExist(folderToListPath);
+  const filesArray = [];
+
+  if (folderExists) {
+    const items = await fs.readdir(folderToListPath, { withFileTypes: true });
+
+    for (let item of items) {
+      filesArray.push(item.name);
+    }
+    console.log(filesArray);
+
+  } else throw new Error ('FS operation failed');
 };
+
+list();
