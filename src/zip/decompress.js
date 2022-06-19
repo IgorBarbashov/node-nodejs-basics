03a69e276a6df5a14f901ac460ab0271219b8584
Igor Barbashov <bis200705@gmail.com>
2022-06-19 22:03:00 +0300

    feat: implement zip functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+import { createGunzip } from 'zlib';
+
+const __dirname = path.resolve(path.dirname(''));
+
 export const decompress = async () => {
-    // Write your code here 
+  const fileToDecompress = fs.createReadStream(path.join(__dirname, 'files/archive.gz'));
+  const archive = createGunzip();
+  const writableStream = fs.createWriteStream(path.join(__dirname, 'files/fileToCompress.txt'), 'utf-8');
+  fileToDecompress.pipe(archive).pipe(writableStream);
 };
+
+decompress();
+
+
+
+// import { createUnzip } from 'zlib';
+// import { pipeline } from 'stream/promises';
+// import { createReadStream, createWriteStream } from 'fs';
+//
+// export const decompress = async () => {
+//   const path = './src/zip/files';
+//   const gzip = createUnzip();
+//   const source = createReadStream(`${path}/archive.gz`);
+//   const dest = createWriteStream(`${path}/fileToCompress.txt`);
+//
+//   await pipeline(source, gzip, dest);
+//
+//   console.log('Pipeline succeeded. Check fileToCompress.txt.');
+// };
+//
+// decompress().then();
